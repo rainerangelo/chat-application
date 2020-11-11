6c36fabb61a9226594bde7839b1933b00b8963d0
Rainer Lim <limrainer.ph@gmail.com>
2020-11-11 13:13:47 -0700

    Make chat application functional

---
+++ index.js
@@ -0,0 +1,62 @@
+const max_number_of_users = 5;
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var user_id_counter = 0;
+
+var status = {
+    users: [],
+    messages: []
+}
+
+class User {
+    constructor(id) {
+        this.id = id;
+        this.active = true;
+    }
+}
+
+class Message {
+    constructor(text, user) {
+        this.text = text;
+        this.user = user;
+    }
+}
+
+app.use(require('express').static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('new user', () => {
+        var user = new User(user_id_counter);
+        status.users.push(user);
+        user_id_counter++;
+
+        socket.emit('new user', user);
+    });
+
+    socket.on('chat message', (data) => {
+        var user = status.users.find(user => user.id === Number(data.id));
+        var message = new Message(data.message, user);
+        status.messages.push(message);
+
+        io.emit('render', status);
+    });
+
+    socket.on('render', () => {
+        io.emit('render', status);
+    });
+
+    socket.on('disconnect', () => {
+        
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ public/js/client.js
@@ -0,0 +1,45 @@
+function get_stored_user_id() {
+    return localStorage.getItem('user_id');
+}
+
+$(function () {
+    var socket = io();
+
+    if (get_stored_user_id()) {
+        console.log("Found stored user ID in local storage");
+    }
+    else {
+        console.log("Did not find stored user ID in local storage");
+        socket.emit('new user');
+    }
+
+    socket.emit('render');
+
+    $('form').submit(function (e) {
+        console.log("In submit, message is " + $('#message').val() + " from " + get_stored_user_id());
+        e.preventDefault();
+        socket.emit('chat message', { message: $('#message').val(), id: get_stored_user_id() });
+        $('#message').val('');
+        return false;
+    })
+
+    socket.on('new user', (user) => {
+        console.log("Store user ID to local storage");
+        localStorage.setItem('user_id', user.id);
+    })
+
+    socket.on('render', (status) => {
+        console.log("In render");
+
+        $('#participants').empty();
+        $('#messages').empty();
+
+        for (let i = 0; i < status.users.length; i++) {
+            $('#participants').append($('<li>').text(status.users[i].id));
+        }
+
+        for (let i = 0; i < status.messages.length; i++) {
+            $('#messages').append($('<li>').text(status.messages[i].user.id + ": " + status.messages[i].text));
+        }
+    });
+});
