87f368fba24ab7be3ee1864335202cd5ec267fda
Rainer Lim <limrainer.ph@gmail.com>
2020-11-12 22:56:28 -0700

    Add comments to JS files to describe their purpose

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+/**
+ * This file includes code for the server of the chat application.
+ *
+ * Author: Rainer Lim
+ */
+
 // Turn off logging
 // console.log = function() {}
 
+// These are for restricting the number of messages and users kept in the server
 const MAX_NUMBER_OF_MESSAGES = 200;
-const MAX_NUMBER_OF_USERS = 5;
+// const MAX_NUMBER_OF_USERS = 5; // TODO: Restrict number of users
+
+// These are for generating 4-digit hexadecimal user IDs for users
 const USERNAME_LENGTH = 4;
 const HEX = '0123456789ABCDEF';
 
+// These are for creating the express application, HTTP server, and Socket IO server
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+// This keeps track of the status of the chat application
 var status = {
     ids: [],
     users: [],
     messages: []
 }
 
+/**
+ * This function generates a user ID for a user who has just joined the chat application
+ */
 function generate_user_id() {
     let user_id = '';
     do {
@@ -26,6 +40,9 @@ function generate_user_id() {
     return user_id;
 }
 
+/**
+ * This function uses usernames to alphabetically sort users in the chat application
+ */
 function sort_users() {
     status.users.sort((first, second) => {
         if (first.username < second.username) {
@@ -38,36 +55,53 @@ function sort_users() {
     })
 }
 
+/**
+ * This function emits a 'render' event to all sockets so that all instances of the chat
+ * application is updated with the latest status
+ */
 function render() {
     io.emit('render', status);
 }
 
+/**
+ * This is the User class, which holds all information related to a user
+ */
 class User {
     constructor(socket_id) {
-        this.socket_id = socket_id;
-        this.id = generate_user_id();
-        this.username = 'User ' + this.id;
-        this.color = 'white';
-        this.active = true;
+        this.socket_id = socket_id;         // Socket ID
+        this.id = generate_user_id();       // User ID
+        this.username = 'User ' + this.id;  // Username
+        this.color = 'white';               // Username color
+        this.active = true;                 // Activity (i.e., online or offline)
     }
 }
 
+/**
+ * This is the Message class, which holds all information related to a message
+ */
 class Message {
     constructor(text, user) {
-        this.text = text;
-        this.user = user;
-        this.timestamp = new Date();
+        this.text = text;               // Message text
+        this.user = user;               // User from whom message was sent
+        this.timestamp = new Date();    // Message timestamp
     }
 }
 
+// Allow express application to use static files in '/public' directory
 app.use(require('express').static(__dirname + '/public'));
 
+// Use '/index.html' as the main page of express application
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// The 'connection' event occurs when a client connects to the server
 io.on('connection', (socket) => {
+
+    // The 'new user' event occurs when a new user, who has not used the application before, joins
     socket.on('new user', () => {
+
+        // Create new user and add to the array of users in status
         var user = new User(socket.id);
         status.users.push(user);
         sort_users();
@@ -77,6 +111,7 @@ io.on('connection', (socket) => {
         render();
     });
 
+    // The 'previous user' event occurs when a user, who has used the application before, rejoins
     socket.on('previous user', (stored_id) => {
         var user = status.users.find(user => user.id === stored_id);
 
@@ -85,6 +120,7 @@ io.on('connection', (socket) => {
             user.active = true;
         }
         else {
+            // Server must have restarted and user's ID is old, so create new user
             var user = new User(socket.id);
             status.users.push(user);
             sort_users();
@@ -95,13 +131,17 @@ io.on('connection', (socket) => {
         render();
     });
 
+    // The 'chat message' event occurs when a new message is sent to the chat application
     socket.on('chat message', (data) => {
         var user = status.users.find(user => user.id === data.id);
 
         if (user) {
+
+            // Create new message and add to the array of messages in status
             var message = new Message(data.message, user);
             status.messages.push(message);
 
+            // Restrict the number of messages kept in the server to MAX_NUMBER_OF_MESSAGES
             if (status.messages.length > MAX_NUMBER_OF_MESSAGES) {
                 status.messages.shift();
             }
@@ -110,6 +150,7 @@ io.on('connection', (socket) => {
         render();
     });
 
+    // The 'update name' event occurs when a user sends the command to change their username
     socket.on('update name', (name) => {
         var user = status.users.find(user => user.socket_id === socket.id);
 
@@ -124,6 +165,7 @@ io.on('connection', (socket) => {
         render();
     });
 
+    // The 'update color' event occurs when a user sends the command to change their username color
     socket.on('update color', (color) => {
         var user = status.users.find(user => user.socket_id === socket.id);
 
@@ -134,10 +176,12 @@ io.on('connection', (socket) => {
         render();
     });
 
+    // The 'render' event occurs when a client is requesting an update to their chat application
     socket.on('render', () => {
         render();
     });
 
+    // The 'disconnect' event occurs when a user disconnects from the chat application
     socket.on('disconnect', () => {
         var user = status.users.find(user => user.socket_id === socket.id);
 
@@ -149,8 +193,10 @@ io.on('connection', (socket) => {
     });
 });
 
+// If available, use value stored in PORT environment variable, otherwise, use port 3000
 const PORT = process.env.PORT || 3000;
 
+// Create listener on specified port
 http.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,38 @@
+/**
+ * This file includes code for the client of the chat application.
+ *
+ * Author: Rainer Lim
+ */
+
 // Turn off logging
 // console.log = function() {}
 
+// This is for converting timestamp differences from milliseconds to hours
 const NUMBER_OF_MILLISECONDS_IN_AN_HOUR = 1000 * 60 * 60;
 
+/**
+ * This function gets the stored user ID from localStorage
+ */
 function get_stored_user_id() {
     return localStorage.getItem('user_id');
 }
 
+/**
+ * This function scrolls down to the bottom of the messages HTMLElement
+ * @param {HTMLElement} messages
+ */
 function scroll_to_bottom(messages) {
     messages.scrollTop = messages.scrollHeight;
 }
 
+/**
+ * This function is the start of the client code for the chat application
+ */
 $(function () {
-    var socket = io();
-    var stored_id = get_stored_user_id();
+    var socket = io();                      // Create the socket and connect to the server
+    var stored_id = get_stored_user_id();   // Get stored user ID
 
+    // If user ID is found, emit a 'previous user' event, otherwise, emit a 'new user' event
     if (stored_id) {
         console.log("Found stored user ID in local storage");
         socket.emit('previous user', stored_id);
@@ -24,48 +42,65 @@ $(function () {
         socket.emit('new user');
     }
 
+    // This handles messages being sent (or submitted) to the chat application
     $('form').submit(function (e) {
         e.preventDefault();
         let received_message = $('#message').val();
 
+        // Process message only when there is at least one non-whitespace character
         if (received_message.trim().length) {
+
+            // Handle username customization command
             if (received_message.startsWith('/name ')) {
                 socket.emit('update name', received_message.split('/name ')[1]);
             }
+
+            // Handle username color customization command
             else if (received_message.startsWith('/color ')) {
                 socket.emit('update color', received_message.split('/color ')[1]);
             }
+
+            // Handle non-command messages
             else {
-                received_message = received_message.replace(':)', '&#128578;');
-                received_message = received_message.replace(':(', '&#128577;');
-                received_message = received_message.replace(':o', '&#128558;');
+                received_message = received_message.replace(':)', '&#128578;'); // Happy face emoji conversion
+                received_message = received_message.replace(':(', '&#128577;'); // Sad face emoji conversion
+                received_message = received_message.replace(':o', '&#128558;'); // Open-mouth face emoji conversion
                 socket.emit('chat message', { message: received_message, id: get_stored_user_id() });
             }
         }
 
+        // Clear message input
         $('#message').val('');
         return false;
     })
 
+    // The 'new user' event occurs when a new user, who has not joined the application before, joins
     socket.on('new user', (user) => {
         console.log("Store user ID to local storage");
+
+        // Once the server has assigned a user ID to the new user, store into localStorage
         localStorage.setItem('user_id', user.id);
     })
 
+    // The 'render' event occurs when a client is requesting an update to their chat application
     socket.on('render', (status) => {
         console.log("In render");
 
         var messages = document.getElementById('grid-item-messages');
 
+        // Determine whether or not the list of messages will scroll down automatically
+        // Only scroll down when the user is at the very bottom of the list of messages
         auto_scroll = messages.scrollTop + messages.clientHeight >= messages.scrollHeight;
 
         $('#messages').empty();
         $('#users').empty();
 
+        // Loop through and display all messages in web page
         for (let i = 0; i < status.messages.length; i++) {
             let timestamp = new Date(status.messages[i].timestamp);
             let difference_in_hours = Math.abs(new Date() - timestamp) / NUMBER_OF_MILLISECONDS_IN_AN_HOUR;
 
+            // If message timestamp is less than a day old, display time only, otherwise, display date only
             if (difference_in_hours < 24) {
                 timestamp = timestamp.toLocaleTimeString('en-US', { hour: "numeric", minute: "numeric" });
             }
@@ -73,13 +108,14 @@ $(function () {
                 timestamp = timestamp.toLocaleDateString('en-US', { year: "numeric", month: "numeric", day: 'numeric' });
             }
 
-            let message_item = $(`<li></li>`);
-            let message_item_username = $(`<p></p>`);
-            let message_item_message = $(`<p class="message-text"></p>`);
+            let message_item = $(`<li></li>`);                              // List item for entire message
+            let message_item_username = $(`<p></p>`);                       // Paragraph for username portion
+            let message_item_message = $(`<p class="message-text"></p>`);   // Paragraph for message text portion
 
             message_item_username.append(`<span style="color: ${status.messages[i].user.color}">${status.messages[i].user.username}</span><span class="timestamp">${timestamp}</span>`);
             message_item_message.append(status.messages[i].text);
 
+            // If message is from main user (i.e., the main user for that chat application instance)
             if (status.messages[i].user.id === get_stored_user_id()) {
                 message_item_username.children('span')[0].append(' (You)');
                 message_item.addClass('message-main-user');
@@ -94,18 +130,23 @@ $(function () {
 
         let number_of_online_users = 0;
 
+        // Loop through and display all users in web page
         for (let i = 0; i < status.users.length; i++) {
+
+            // If user is not active (i.e., offline), continue
             if (!status.users[i].active) {
                 continue;
             }
 
+            // Count number of online users
             number_of_online_users++;
 
-            let user_item = $(`<li></li>`);
-            let user_item_username = $(`<p></p>`);
+            let user_item = $(`<li></li>`);         // List item for entire user
+            let user_item_username = $(`<p></p>`);  // Paragraph for username portion
 
             user_item_username.append(`<span style="color: ${status.users[i].color}">${status.users[i].username}</span>`);
 
+            // If user is main user (i.e., the main user for that chat application instance)
             if (status.users[i].id === get_stored_user_id()) {
                 user_item_username.children('span')[0].append(' (You)');
             }
@@ -115,9 +156,11 @@ $(function () {
             $('#users').append(user_item);
         }
 
+        // Display number of online users
         $('#number-of-online-users').empty();
         $('#number-of-online-users').append(` (${number_of_online_users}/${status.users.length})`);
 
+        // If true, automatically scroll down to the bottom of the list of messages
         if (auto_scroll) {
             scroll_to_bottom(messages);
         }
